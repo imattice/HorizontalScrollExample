c42a7a8f2004d186efdb66698981023ffd325f3f
Ike Mattice <ikemattice@icloud.com>
2022-02-06 10:21:02 -0800

    Initial Commit

---
+++ horizontal-scroll-test/horizontal_scroll_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  horizontal_scroll_testApp.swift
+//  horizontal-scroll-test
+//
+//  Created by Ike Mattice on 2/6/22.
+//
+
+import SwiftUI
+
+@main
+struct horizontal_scroll_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
